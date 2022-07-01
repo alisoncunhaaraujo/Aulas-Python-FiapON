0a97d8446e01b468b02908420a6bfc8339abb994
Your NameAlison <alisonfoxnet@gmail.com>
2022-07-01 13:50:43 -0300

    Atualizando

--- OT/Decisoes/Decisao_For.py
+++ OT/Decisoes/Decisao_For.py
@@ -1,1 +1,4 @@
-tabuada = int(input("Digite um numero para exibir a tabuada: "))
+tabuada = int(input("Digite um número para exibir a tabuada: "))
+print("Tabuada do número ", tabuada)
+for valor in range(1, 11, 1):
+    print(str(tabuada) + " x " + str(valor) + " = " + str((tabuada*valor)))
